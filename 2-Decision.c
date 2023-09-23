6aeb5df2d105f31bc570ac166fa5e2aa62e6186d
Srajan Rai <ftsrajan@gmail.com>
2023-09-23 09:18:58 +0530

    Create 2-Decision.c

---
+++ 2-Decision.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // system("cls");
+    int a = 3;
+    if(printf("0")) printf("1");
+    if(printf("2")) printf("3");
+    if(-5) printf("4");
+    if(3+2%5) printf("5");
+    if(a=0) printf("6");
+    if(a=1) printf("7");
+    printf("%d", a);
+    if(a==1);
+        printf("9");
+    return 0;
+}
