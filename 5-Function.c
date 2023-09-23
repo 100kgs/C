b0c844afe9f7547562a299077c82d8f962b1ce72
Srajan Rai <ftsrajan@gmail.com>
2023-09-24 01:01:47 +0530

    Create 5-Function.c

---
+++ 5-Function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // system("cls");
+
+    // int a;
+    // printf("Num: ");
+    // scanf("%d", &a);
+    // if(a==0) return 0;
+    // printf("Num returned: %d\n", main());
+
+    return 0;
+}
